603ac5f3a4a546979005777c5928a05a2d8244a1
Thomas833 <tpgraham@mines.edu>
2026-04-23 19:48:49 -0500

    solved 3sum problem with some help

---
+++ #15 3Sum/explanation.md
@@ -0,0 +1,1 @@
+https://leetcode.com/problems/3sum/solutions/5055810/video-two-pointer-solution-by-niits-cl7y/?envType=study-plan-v2&envId=top-interview-150
---
+++ #15 3Sum/solution.py
@@ -0,0 +1,22 @@
+def threeSome(nums: list[int]) -> list[list[int]]:
+    output = []
+    nums.sort()
+    for i in range(len(nums)):
+        if i > 0 and nums[i] == nums[i-1]:
+                continue
+        j = i + 1
+        k = len(nums) - 1
+        while j < k:
+            if nums[i] + nums[j] + nums[k] > 0:
+                k -= 1
+            elif nums[i] + nums[j] + nums[k] < 0:
+                j += 1
+            else:
+                output.append([nums[i], nums[j], nums[k]])
+                j += 1
+                while nums[j] == nums[j-1] and j < k:
+                    j += 1
+    return output
+
+# [-4,-1,-1,0,1,1,2]
+print(threeSome([-4,-1,-1,0,1,1,2]))
