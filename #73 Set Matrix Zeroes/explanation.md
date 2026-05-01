b8c917eab91017c4df5d9a3ef0ccdcf4eed2bfbf
Thomas833 <tpgraham@mines.edu>
2026-04-30 19:17:20 -0500

    solved the 73-set-matrix-zeroes problem | 4/10 | read over the explanation for a refresher

--- #73 Set Matrix Zeroes/explanation.md
+++ #73 Set Matrix Zeroes/explanation.md
@@ -1,1 +1,17 @@
-i am thinking to use two hashmaps to determine if a given cell shares the same i or j with any of the zeros. 
+matrix = [[0,1,2,0],
+          [3,4,5,2],
+          [1,3,1,5]]
+
+check if first row or first col have 0s. if they do, set boolean to true
+
+iterate through matrix starting from index 1,1 to m,n. if the cell has a 0 in it, set
+matrix[row][0] to 0 and matrix[0][col] to 0.
+
+iterate through and change row to 0s if matrix[row][0] is 0
+do the same for cols
+
+lastly, check if the booleans are true. if so, change first row and/or first col to all zeros
+
+return matrix
+
+performance is n x m with O(1) space
--- #73 Set Matrix Zeroes/solution.py
+++ #73 Set Matrix Zeroes/solution.py
@@ -1,6 +1,54 @@
-def solution():
-    # write code here
-    pass
+def solution(matrix):
+    first_row_has_0 = False
+    first_col_has_0 = False
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    # check if first row has 0s
+    for j in range(cols):
+        if matrix[0][j] == 0:
+            first_row_has_0 = True
+            break
+    
+    # check if first col has 0s
+    for i in range(rows):
+        if matrix[i][0] == 0:
+            first_col_has_0 = True
+            break
+
+    # walk through matrix and set row/col to 0 if val is 0
+    for i in range(1, rows):
+        for j in range(1, cols):
+            if matrix[i][j] == 0:
+                matrix[i][0] = 0
+                matrix[0][j] = 0
+                
+    # set rows to 0 if matrix[row][0] is 0
+    for i in range(1,rows):
+        if matrix[i][0] == 0:
+            for j in range(cols):
+                matrix[i][j] = 0
+
+    # set cols to 0 if  matrix[0][col] is 0
+    for j in range(1,cols):
+        if matrix[0][j] == 0:
+            for i in range(rows):
+                matrix[i][j] = 0
+    
+    # if first row has 0, set row to 0s
+    if first_row_has_0:
+        for j in range(cols):
+            matrix[0][j] = 0
+    
+    # if first col has 0s in it, set all to 0s
+    if first_col_has_0:
+        for i in range(rows):
+            matrix[i][0] = 0
+
+    return matrix
 
 
-print(solution())
+
+
+matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+print(solution(matrix))
