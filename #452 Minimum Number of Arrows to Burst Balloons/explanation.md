1fc8882716713a0f1fbfe461b5b3449a7f4a9a17
Thomas833 <tpgraham@mines.edu>
2026-05-13 20:24:57 -0500

    solved the 452-minimum-number-of-arrows-to-burst-balloons problem | 3/10 | read over the explanation for a refresher

--- #452 Minimum Number of Arrows to Burst Balloons/explanation.md
+++ #452 Minimum Number of Arrows to Burst Balloons/explanation.md
@@ -1,3 +1,13 @@
 # #452 Minimum Number of Arrows to Burst Balloons - Explanation
 
-Write your explanation here.
+sort list
+set prevInt to first element
+count = 0
+iterate through list starting at second interval
+    # if curInt[0] is greater than prevInt[1], add 1 to count since no overlap. set prevInt to curInt
+
+l   # if curInt[0] is less than or equal to prevInt[1], we have overlap.
+
+O(nlogn) and O(1)
+
+
--- #452 Minimum Number of Arrows to Burst Balloons/solution.py
+++ #452 Minimum Number of Arrows to Burst Balloons/solution.py
@@ -1,6 +1,26 @@
-def solution():
-    # write code here
-    pass
+def solution(points: list[list[int]])->bool:
+    count = 1
+    points.sort()
+    end = points[0][1]
+    for point in points[1:]:
+        #no overlap
+        if point[0] > end:
+            count+=1
+            end = point[1]
+        else: #overlap exists
+            end = min(end,point[1])
+    return count
 
+points = [[1,2],[2,3],[3,4],[4,5]]
+print(solution(points))
 
-print(solution())
+def efficientSolution(points: list[list[int]])->bool:
+    points.sort(key=lambda x:x[1])
+    arrows = 1
+    last_end = points[0][1]
+
+    for start, end in points:
+        if start>last_end:
+            arrows += 1
+            last_end = end
+    return arrows
