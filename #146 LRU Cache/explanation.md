2065f20b9f16998b4d2a8a05cb1f574d37b727b1
Thomas833 <tpgraham@mines.edu>
2026-06-02 20:34:53 -0500

    solved the 146-lru-cache problem | 7/10 | read over the explanation for a refresher

--- #146 LRU Cache/explanation.md
+++ #146 LRU Cache/explanation.md
@@ -3,21 +3,29 @@
 counter to track length of linked list
 have hashmap store key as key, and node as value with value given inside
 
-length = 1
-capacity = 3
-hm = []
 
+capacity = 2
+cache = {2:2,1:1}
 
-h                            t
-dummy <-> 1 dummy
+put(1,1)
+put(2,2)
+get(1)
+
+              
+head <-> 2 -> 1 <-> tail
 
 if counter less than capacity, add it to head of doubly linked list and map and increase counter
 
+get(key)
+check if key in cache. 
+if yes, return node's value and update node to head of list
+if no, return -1
 
+put(key, value)
 if in cache:
-        update node's value and pointers
+        update node's value, adjust neighbor's pointers, and bring node to head of list
 else:
-    if length < capacity:
-        add node to head of list and add node to map and increase length
-    else:
-        remove oldest node from list and cache and add node to head of list and add to cache
+    if length(cache) < capacity:
+        add node to head of list and add node to cache
+    else: length == capacity and key,value not in cache
+        remove node previous to tail (oldest node), update its neighbor's pointers, remove from cache, and create new node and add it to head of list and add it to map
--- #146 LRU Cache/solution.py
+++ #146 LRU Cache/solution.py
@@ -25,8 +25,8 @@ class LRUCache:
     def put(self, key: int, value: int) -> None:
         if key not in self.cache:
             if len(self.cache) == self.capacity: # replace oldest node with new node
-                self.removeTail()
-                self.addNode(key, value) # create node and add to head of linked list
+                self.removeOldest()
+                self.addNode(key, value) 
             else: # length < capacity, simply add to head of list and add to cache
                 self.addNode(key,value)
         else: # in cache, so update node's value and pointers
@@ -34,32 +34,36 @@ class LRUCache:
             node.val = value
             self.makeNodeMostRecent(node) # update pointers
 
-    # add new node to linkedList and cache
+    # add new node to head of linkedList and cache
     def addNode(self,key:int, value:int)->None:
         node = self.Node(key,value)
         self.cache[key] = node
+        # set node's, dummy_head, and old head's pointers
         node.next = self.dummy_head.next
         node.prev = self.dummy_head
+        self.dummy_head.next.prev = node # update old head of list to point to new head of list
         self.dummy_head.next = node
-        if not self.dummy_tail.prev: # if adding first node
-            self.dummy_tail.prev = node
         
-    
-    def removeTail(self)->None:
-        tail = self.dummy_tail.prev
-        tail.prev.next = tail.next
-        tail.next.prev = tail.prev # update dummy_tail
-        del self.cache[tail.key]
+    # remove oldest node from list and cache
+    def removeOldest(self)->None:
+        oldest = self.dummy_tail.prev
+        #update neighbor's pointers
+        oldest.prev.next = oldest.next
+        oldest.next.prev = oldest.prev # update dummy_tail
+        #remove from cache
+        del self.cache[oldest.key]
 
+    # for making existing nodes move to the head of the list
     def makeNodeMostRecent(self,node:Node)->None:
         # detach node 
         node.prev.next = node.next
         node.next.prev = node.prev
         
+        # set node's, dummy_head, and old head's pointers
         node.next = self.dummy_head.next
         node.prev = self.dummy_head
-        if not self.dummy_tail.prev: # if adding first node
-            self.dummy_tail.prev = node
+        self.dummy_head.next.prev = node # update old head of list to point to new head of list
+        self.dummy_head.next = node
 
 
 
